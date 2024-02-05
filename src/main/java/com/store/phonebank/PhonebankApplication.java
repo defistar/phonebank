be9cb0cc87a37354e0bf9e4cb35ee5c14da52474
kanth <kanth.explorer@gmail.com>
2024-02-06 00:01:32 +0530

    first commit

---
+++ src/main/java/com/store/phonebank/PhonebankApplication.java
@@ -0,0 +1,10 @@
+package com.store.phonebank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class PhonebankApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(PhonebankApplication.class, args);
+	}
+}
